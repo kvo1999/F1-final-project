1525b723cf8688d201da207a13218747bcb04074
lorianchester <lec128@georgetown.edu>
2022-04-27 17:51:15 -0400

    Add pitstop functions to all_functions.py

--- app/all_functions.py
+++ app/all_functions.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import statistics
+import datetime
 
 def average_finish(driver_lname):
     ergast_url = f"https://ergast.com/api/f1/2021/drivers/{driver_lname}/results.json"
@@ -110,3 +111,53 @@ def qualifying_time(circuit):
     key_min = min(qual_times.keys(), key=(lambda k: qual_times[k]))
     print("Fastest Qualifying Time:", min(qual_times, key=qual_times.get), qual_times[key_min])
 
+def avg_pitstop_time(racer, option):
+
+  if option.upper() == "R":
+
+    round = input("What round do you want to see data for? ")
+
+    pit_stop_url = "https://ergast.com/api/f1/2021/" + str(round) + "/pitstops.json"
+  
+    response = requests.get(pit_stop_url)
+
+    pitstop_data = json.loads(response.text)
+
+    for pitstop in pitstop_data["MRData"]["RaceTable"]["Races"][0]["PitStops"]:
+      if pitstop["driverId"] == racer:
+        avg_time = pitstop["duration"]
+
+  elif option.upper() == "S":
+    total_rounds = 23
+    round = 1
+    stop_counter = 0
+    season_pitstop_time = 0
+
+    while round < total_rounds:
+      pit_stop_url = "https://ergast.com/api/f1/2021/" + str(round) + "/drivers/" + racer.lower() + "/pitstops.json"
+      response = requests.get(pit_stop_url)
+      driver_data = json.loads(response.text)
+
+      if int(driver_data["MRData"]["total"]) > 0:
+        for stop in driver_data["MRData"]["RaceTable"]["Races"][0]["PitStops"]:
+          time = to_seconds(stop["duration"])
+          season_pitstop_time = season_pitstop_time + time
+          stop_counter = stop_counter + 1
+  
+      round = round + 1
+
+    avg_time = season_pitstop_time / stop_counter
+
+  return avg_time
+
+def to_seconds(time):
+  if ":" in time:
+    time = datetime.datetime.strptime(time, "%M:%S.%f")
+    minutes = time.minute
+    seconds = time.second
+    microseconds = time.microsecond
+    time = (minutes * 60) + seconds + (microseconds * 0.000001)
+  else:
+    time = float(time)
+
+  return time
