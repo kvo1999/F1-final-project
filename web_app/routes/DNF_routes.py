dff1560cca267bfc70ec95dcea766fc2e6ec235c
kvo1999 <kav50@georgetown.edu>
2022-04-27 17:45:40 -0400

    update routes

--- web_app/routes/DNF_routes.py
+++ web_app/routes/DNF_routes.py
@@ -1,13 +1,23 @@
 # web_app/routes/DNF_routes.py
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, jsonify
 
 from app.all_functions import reason_DNF
 
 DNF_routes = Blueprint("DNF_routes", __name__)
 
 @DNF_routes.route("/DNF")
-def index():
-    print("HOME...")
-    return "Welcome Home"
-    #return render_template("home.html")
+def dnf_f1_api():
+    print("DRIVER STATS")
+
+    url_params = dict(request.args)
+    print("URL PARAMS:" , url_params)
+
+    driver_lname = url_params.get("driver_lname") or "hamilton"  
+
+    results = reason_DNF(driver_lname)
+
+    if results:
+        return jsonify(results)
+    else:
+        return jsonify({"message: Invalid driver ID, please try again."}), 404
