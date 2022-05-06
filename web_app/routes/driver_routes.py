d2190a51dd4f1c13e5293c70d3a9149b111b6140
annava28 <97325167+annava28@users.noreply.github.com>
2022-05-05 20:40:00 -0400

    front end formatting

--- web_app/routes/driver_routes.py
+++ web_app/routes/driver_routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, request, render_template
 from app.all_functions import master_function
+from app.all_functions import driverinfolist
 
 driver_routes = Blueprint("DRIVER", __name__)
 
 @driver_routes.route("/drivers/<driver_lname>")
 def plswork(driver_lname):
     results= master_function(driver_lname)
-    return render_template("result_layout.html", results=results, driver_lname=driver_lname)
+    driver=driverinfolist(driver_lname)
+    return render_template("result_layout.html", results=results, driver_lname=driver_lname, driver=driver)
 
 
 @driver_routes.route("/driver/form")
@@ -14,3 +16,4 @@ def index():
     print("driver form")
     #return "Welcome Home"
     return render_template("driver_form.html")
+
