8fe73c468fce8dced014d427d998a608d0246f84
kvo1999 <kav50@georgetown.edu>
2022-05-06 02:06:38 -0400

    final update

--- web_app/routes/driver_routes.py
+++ web_app/routes/driver_routes.py
@@ -10,12 +10,12 @@ driver_routes = Blueprint("DRIVER", __name__)
 
 #this route runs all the functions in the web app, pulls data from API which takes a long time 
 
-#@driver_routes.route("/drivers/<driver_lname>")
-#def plswork(driver_lname):
-    #print("driver results...")
-    #results= master_function(driver_lname)
-    #driver=driverinfolist(driver_lname)
-    #return render_template("result_layout.html", results=results, driver_lname=driver_lname, driver=driver)
+@driver_routes.route("/drivers/<driver_lname>")
+def plswork(driver_lname):
+    print("driver results...")
+    results= master_function(driver_lname)
+    driver=driverinfolist(driver_lname)
+    return render_template("result_layout.html", results=results, driver_lname=driver_lname, driver=driver)
 
 
 @driver_routes.route("/driver/form")
@@ -24,12 +24,12 @@ def index():
     #return "Welcome Home"
     return render_template("driver_form.html")
 
-
-@driver_routes.route("/drivers/<driver_lname>")
-def altroute(driver_lname):
-    print("driver results...")
-    results = processed_data(driver_lname)
-    driver=driverinfolist(driver_lname)
-    return render_template("resultcsv_layout.html", results=results, driver_lname=driver_lname, driver=driver)
+#to load from CSV
+#@driver_routes.route("/drivers/<driver_lname>")
+#def altroute(driver_lname):
+    #print("driver results...")
+    #results = processed_data(driver_lname)
+    #driver=driverinfolist(driver_lname)
+    #return render_template("resultcsv_layout.html", results=results, driver_lname=driver_lname, driver=driver)
 
 
